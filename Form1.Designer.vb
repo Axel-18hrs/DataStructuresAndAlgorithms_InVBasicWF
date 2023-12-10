8b3014509f9712022d1b1d8e8a8e9564dba49aca
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-12-10 14:20:57 -0600

    .

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,19 +22,79 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        ListsToolStripMenuItem = New ToolStripMenuItem()
+        SimpleListToolStripMenuItem = New ToolStripMenuItem()
+        CircularListToolStripMenuItem = New ToolStripMenuItem()
+        DoublyListToolStripMenuItem = New ToolStripMenuItem()
+        CircularDoublyListToolStripMenuItem = New ToolStripMenuItem()
+        MenuStrip1.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.ImageScalingSize = New Size(20, 20)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {ListsToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(1127, 28)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' ListsToolStripMenuItem
+        ' 
+        ListsToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {SimpleListToolStripMenuItem, CircularListToolStripMenuItem, DoublyListToolStripMenuItem, CircularDoublyListToolStripMenuItem})
+        ListsToolStripMenuItem.Name = "ListsToolStripMenuItem"
+        ListsToolStripMenuItem.Size = New Size(51, 24)
+        ListsToolStripMenuItem.Text = "Lists"
+        ' 
+        ' SimpleListToolStripMenuItem
+        ' 
+        SimpleListToolStripMenuItem.Name = "SimpleListToolStripMenuItem"
+        SimpleListToolStripMenuItem.Size = New Size(224, 26)
+        SimpleListToolStripMenuItem.Text = "Simple List"
+        ' 
+        ' CircularListToolStripMenuItem
+        ' 
+        CircularListToolStripMenuItem.Name = "CircularListToolStripMenuItem"
+        CircularListToolStripMenuItem.Size = New Size(224, 26)
+        CircularListToolStripMenuItem.Text = "Circular List"
+        ' 
+        ' DoublyListToolStripMenuItem
+        ' 
+        DoublyListToolStripMenuItem.Name = "DoublyListToolStripMenuItem"
+        DoublyListToolStripMenuItem.Size = New Size(224, 26)
+        DoublyListToolStripMenuItem.Text = "Doubly List"
+        ' 
+        ' CircularDoublyListToolStripMenuItem
+        ' 
+        CircularDoublyListToolStripMenuItem.Name = "CircularDoublyListToolStripMenuItem"
+        CircularDoublyListToolStripMenuItem.Size = New Size(224, 26)
+        CircularDoublyListToolStripMenuItem.Text = "Circular Doubly List"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1127, 527)
+        Controls.Add(MenuStrip1)
         FormBorderStyle = FormBorderStyle.FixedSingle
+        MainMenuStrip = MenuStrip1
         MaximizeBox = False
         Name = "Form1"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ListsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SimpleListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CircularListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DoublyListToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CircularDoublyListToolStripMenuItem As ToolStripMenuItem
 
 End Class
