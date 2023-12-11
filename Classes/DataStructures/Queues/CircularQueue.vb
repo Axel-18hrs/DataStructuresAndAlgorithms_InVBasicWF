64e5f47187af416d97e850738e7bac452ebd6d71
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-12-10 22:25:02 -0600

    .

--- Classes/DataStructures/Queues/CircularQueue.vb
+++ Classes/DataStructures/Queues/CircularQueue.vb
@@ -1,4 +1,6 @@
-﻿Friend Class CircularQueue(Of T)
+﻿Imports System.Windows.Forms
+
+Friend Class CircularQueue(Of T)
     Implements ImethodQueues(Of T)
 
     Private myCircularQueue As T()
@@ -8,13 +10,15 @@
 
     Public Sub New(size As Integer)
         myCircularQueue = New T(size - 1) {}
-        Me.capacity = size
-        front = rear = -1
+        capacity = size
+        front = -1
+        rear = -1
     End Sub
 
     Public Sub Enqueue(value As T) Implements ImethodQueues(Of T).Enqueue
         If IsEmpty() Then
-            front = rear = 0
+            front = 0
+            rear = 0
             myCircularQueue(rear) = value
             Console.WriteLine($"Enqueued: {value}")
             Return
@@ -32,11 +36,11 @@
     End Sub
 
     Public Sub Enqueue(value As T, capacity As Integer) Implements ImethodQueues(Of T).Enqueue
-
+        ' Implementation for Enqueue with custom capacity can be added here if needed.
     End Sub
 
     Public Sub EnqueueRear(value As T) Implements ImethodQueues(Of T).EnqueueRear
-
+        ' Implementation for EnqueueRear can be added here if needed.
     End Sub
 
     Public Sub Dequeue() Implements ImethodQueues(Of T).Dequeue
@@ -47,15 +51,18 @@
         End If
 
         If front = rear Then
-            front = rear = -1
+            Console.WriteLine($"Dequeued: {myCircularQueue(front)}")
+            front = -1
+            rear = -1
             Return
         End If
 
+        Console.WriteLine($"Dequeued: {myCircularQueue(front)}")
         front = (front + 1) Mod capacity
     End Sub
 
     Public Sub DequeueRear() Implements ImethodQueues(Of T).DequeueRear
-
+        ' Implementation for DequeueRear can be added here if needed.
     End Sub
 
     Public Sub Peek() Implements ImethodQueues(Of T).Peek
@@ -65,11 +72,12 @@
             Return
         End If
 
+        Console.WriteLine($"Front element: {myCircularQueue(front)}")
         MessageBox.Show($"Front element: {myCircularQueue(front)}", "-*-*-*-", MessageBoxButtons.OK, MessageBoxIcon.Information)
     End Sub
 
     Public Sub PeekRear() Implements ImethodQueues(Of T).PeekRear
-
+        ' Implementation for PeekRear can be added here if needed.
     End Sub
 
     Public Iterator Function Display() As IEnumerable(Of String) Implements ImethodQueues(Of T).Display
@@ -82,10 +90,10 @@
         Console.Write("Circular Queue elements: ")
         Dim i As Integer = front
         Do
-            Yield $"{myCircularQueue(i)} "
+            Yield myCircularQueue(i).ToString()
             Console.Write($"{myCircularQueue(i)} ")
             i = (i + 1) Mod capacity
-        Loop While i <> (rear + 1) Mod capacity
+        Loop While i Mod capacity <> (rear + 1) Mod capacity
         Console.WriteLine()
     End Function
 
@@ -104,4 +112,5 @@
     Public Function IsEmpty() As Boolean
         Return front = -1 AndAlso rear = -1
     End Function
+
 End Class
